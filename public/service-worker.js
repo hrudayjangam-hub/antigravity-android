04da59aad1cc44cb1f7d959ff518a0476fef2c12
hrudayjangam-hub <hrudayjangam@gmail.com>
2026-03-04 23:40:25 +0530

    Update Service Worker to v2 to clear bad old cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ecotracker-v1';
+const CACHE_NAME = 'ecotracker-v2';
 const ASSETS = [
     'standalone_eco_tracker.html',
     'manifest.json',
@@ -8,9 +8,25 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
@@ -20,3 +36,4 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+
